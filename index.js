06b4dde29a7b2a1b604c96e388179eda4c644de2
mohamedosama2 <latokalatohamato@gmail.com>
2022-10-14 01:04:56 +0200

    auth

--- index.js
+++ index.js
@@ -1,3 +1,117 @@
-import start from './js/Components/App';
+// In index.js of a new project
+import React, { useEffect, useState } from 'react'
+import { View, Text, Button, StyleSheet, TouchableOpacity } from 'react-native'
+import { Navigation } from 'react-native-navigation'
+import AsyncStorage from '@react-native-async-storage/async-storage'
+const isLogged = 'isLogged'
+const loginRoot = {
+  root: {
+    stack: {
+      children: [
+        {
+          component: {
+            name: 'Login'
+          }
+        }
+      ]
+    }
+  }
+}
+function Home(props) {
+  useEffect(() => {
+    const listener = {
+      componentDidAppear: () => {
+        console.log('RNN', `componentDidAppear`)
+      },
+      componentDidDisappear: () => {
+        console.log('RNN', `componentDidDisappear`)
+      }
+    }
+    // Register the listener to all events related to our component
+    const unsubscribe = Navigation.events().registerComponentListener(listener, props.componentId)
+    return () => {
+      // Make sure to unregister the listener during cleanup
+      unsubscribe.remove()
+    }
+  }, [])
 
-start();
+  return (
+    <View>
+      <Text>Home Secreen</Text>
+      <TouchableOpacity
+        onPress={async () => {
+          console.log('Auth')
+        }}>
+        <Text>Out</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+function Setting(props) {
+  console.log('Setting', props)
+  return (
+    <View>
+      <Text>Setting Secreen</Text>
+    </View>
+  )
+}
+Setting.options = {
+  bottomTab: {
+    text: 'Settings'
+  }
+}
+Home.options = {
+  bottomTab: {
+    text: 'Home'
+  }
+}
+function Login() {
+  return (
+    <View>
+      <Text>Login Screen Secreen</Text>
+      <TouchableOpacity
+        onPress={async () => {
+          Navigation.setRoot(mainRoot)
+        }}>
+        <Text>Authenticate</Text>
+      </TouchableOpacity>
+    </View>
+  )
+}
+Navigation.registerComponent('Home', () => Home)
+Navigation.registerComponent('Setting', () => Setting)
+Navigation.registerComponent('Login', () => Login)
+
+const mainRoot = {
+  root: {
+    bottomTabs: {
+      children: [
+        {
+          stack: {
+            children: [{ component: { name: 'Home' } }]
+          }
+        },
+        {
+          stack: {
+            children: [
+              {
+                component: {
+                  name: 'Setting',
+                  id: 'PROFILE_SCREEN_ID',
+                  passProps: {
+                    name: 'John Doe',
+                    status: 'online'
+                  }
+                }
+              }
+            ]
+          }
+        }
+      ]
+    }
+  }
+}
+
+Navigation.events().registerAppLaunchedListener(async () => {
+  Navigation.setRoot(loginRoot)
+})
--- js/Components/App.tsx
+++ js/Components/App.tsx
@@ -1,28 +1,89 @@
-import { useEffect } from 'react'
-import { Navigation } from 'react-native-navigation'
-import HomeScreen from './HomeScreen'
+// In index.js of a new project
+import React from 'react';
+import { View, Text, Button, StyleSheet } from 'react-native';
+import { Navigation } from 'react-native-navigation';
 
-export default function start(): void {
-  Navigation.registerComponent('HomeScreen', () => HomeScreen)
-  Navigation.events().registerAppLaunchedListener(() => {
-    Navigation.setRoot({
-      root: {
-        stack: {
-          id: 'mainStack',
-          children: [
-            {
-              component: {
-                name: 'HomeScreen',
-                options: {
-                  topBar: {
-                    visible: false
-                  }
+// Home screen declaration
+const HomeScreen = (props:any) => {
+  return (
+    <View style={styles.root}>
+      <Text>Hello React Native Navigation 👋</Text>
+      <Button
+        title='Push Settings Screen'
+        color='#710ce3'
+        onPress={() => Navigation.push(props.componentId, {
+          component: {
+            name: 'Settings',
+            options: {
+              topBar: {
+                title: {
+                  text: 'Settings'
                 }
               }
             }
-          ]
-        }
-      }
-    })
-  })
+          }
+        })}/>
+    </View>
+  );
+};
+HomeScreen.options = {
+  topBar: {
+    title: {
+      text: 'Home',
+    }
+  }
+};
+
+// Settings screen declaration - this is the screen we'll be pushing into the stack
+const SettingsScreen = () => {
+  return (
+    <View style={styles.root}>
+      <Text>Settings Screen</Text>
+    </View>
+  );
 }
+
+Navigation.registerComponent('Home', () => HomeScreen);
+Navigation.registerComponent('Settings', () => SettingsScreen);
+
+
+Navigation.setDefaultOptions({
+  statusBar: {
+    backgroundColor: '#4d089a'
+  },
+  topBar: {
+    title: {
+      color: 'white'
+    },
+    backButton: {
+      color: 'white'
+    },
+    background: {
+      color: '#4d089a'
+    }
+  }
+});
+Navigation.events().registerAppLaunchedListener(async () => {
+  Navigation.setRoot({
+    root: {
+      stack: {
+        children: [
+          {
+            component: {
+              name: 'Home'
+            }
+          }
+        ]
+      }
+    }
+  });
+});
+
+const styles = StyleSheet.create({
+  root: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    backgroundColor: 'whitesmoke'
+  }
+});
